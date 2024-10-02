7aff233d8ab812d6a12d3c613da369d1d763e041
datadojoo <datadojoo@hotmail.com>
2024-10-02 20:52:25 +0530

    intial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Suresh",
+    author_email="datadojoo@gmail.com",
+    packages=find_packages()
+)
